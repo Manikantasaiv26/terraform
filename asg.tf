47d31dbd2a341117423a05d4accdfcfee7e1c9b0
Manikantasaiv26 <103366287+Manikantasaiv26@users.noreply.github.com>
2022-11-16 16:09:29 +0530

    Create asg.tf

---
+++ asg.tf
@@ -0,0 +1,106 @@
+module "asg" {
+    source = "terraform-aws-modules/autoscaling/aws"
+
+    # Autoscaling group
+    name = "Mani-asg"
+
+    min_size                  = 1
+    max_size                  = 1
+    desired_capacity          = 1
+    wait_for_capacity_timeout = 0
+    health_check_type         = "EC2"
+    vpc_zone_identifier       = module.vpc.public_subnets
+
+    initial_lifecycle_hooks = [
+    {
+    name                  = "ghosts"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+    name                  = "ghosts"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 180
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+]
+
+    instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+    checkpoint_delay       = 600
+    checkpoint_percentages = [35, 70, 100]
+    instance_warmup        = 300
+    min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+}
+
+    # Launch template
+    launch_template_name        = "sai-asg"
+    launch_template_description = "Launch template leela"
+    update_default_version      = true
+
+    image_id          = "ami-08c40ec9ead489470"
+    instance_type     = "t3.micro"
+    ebs_optimized     = true
+    enable_monitoring = true
+
+    # IAM role & instance profile
+    create_iam_instance_profile = true
+    iam_role_name               = "sai-asg"
+    iam_role_path               = "/ec2/"
+    iam_role_description        = "IAM role leela"
+    iam_role_tags = {
+    CustomIamRole = "Yes"
+    }
+    iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+
+    block_device_mappings = [
+    {
+    # Root volume
+    device_name = "/dev/xvda"
+    no_device   = 0
+    ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+        }
+    }
+    ]
+
+    network_interfaces = [
+    {
+        delete_on_termination = true
+        description           = "eth0"
+        device_index          = 0
+        security_groups       = [module.sg.security_group_id]
+    }
+    ]
+
+    # placement = {
+    #   availability_zone = "us-east-1b"
+    # }
+
+    tag_specifications = [
+    {
+        resource_type = "instance"
+        tags          = { WhatAmI = "Instance" }
+    },
+    {
+        resource_type = "volume"
+        tags          = { WhatAmI = "Volume" }
+    }
+    ]
+
+    tags = {
+    Environment = "dev"
+    Project     = "megasecret"
+    }
+}
